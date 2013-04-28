71f53d52d71b7b00930b15cd31b4323b4f9671e8
ChallenHB <crnhb38@gmail.com>
2013-04-28 03:03:35 -0400

    Making RTSMessage serializable

--- src/vooga/rts/networking/communications/gamemessage/RTSMessage.java
+++ src/vooga/rts/networking/communications/gamemessage/RTSMessage.java
@@ -2,9 +2,14 @@ package vooga.rts.networking.communications.gamemessage;
 
 import vooga.rts.action.InteractiveAction;
 import vooga.rts.commands.Command;
+import vooga.rts.networking.communications.IMessage;
 
-public class RTSMessage extends GameMessage {
+public class RTSMessage extends GameMessage implements IMessage {
 
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4741900659154102286L;
     private Command myCommand;
     private int myUnitId;
     
